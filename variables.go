401f2a0ca10cc518c6f317630e9e0cb929535d8d
SrAlderson <srgonzalez1997@hotmail.com>
2021-07-19 22:04:24 -0500

    variables

---
+++ variables.go
@@ -0,0 +1,25 @@
+// Librerias
+package main
+
+import "fmt"
+
+// Funciones
+func main() {
+
+	// Declaracion de variables
+	var x int
+
+	// Asignacion
+	x = 1
+
+	// Tipado dinamico (recomendado)
+
+	y := 14
+	cadena := "Gio"
+
+	// Resultado
+	fmt.Println(x)
+	fmt.Println(cadena)
+	fmt.Println(y)
+
+}
